dd706af409dc4d8917c4a01e0265a3fa1e58db0f
qwerty3248 <jesuslosada4484@gmail.com>
2024-03-10 18:57:52 +0100

    No se me ocurre nada de nada

--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -1,6 +1,7 @@
 #ifndef COMPORTAMIENTOJUGADOR_H
 #define COMPORTAMIENTOJUGADOR_H
 
+#include <queue>
 #include "comportamientos/comportamiento.hpp"
 using namespace std;
 
@@ -10,6 +11,7 @@ struct state
   int col;
   Orientacion brujula;
 };
+const int MAX = 99;
 
 class ComportamientoJugador : public Comportamiento{
 
@@ -17,11 +19,28 @@ class ComportamientoJugador : public Comportamiento{
     ComportamientoJugador(unsigned int size) : Comportamiento(size){
       // Constructor de la clase
       // Dar el valor inicial a las variables de estado
-      current_state.fil=99;
-      current_state.col=99;
+      current_state.fil=MAX;
+      current_state.col=MAX;
       current_state.brujula=norte;
-      girar_derecha=false;
+      girar_derecha=true;
+      girar_45_grados = true;
       bien_situado = false;
+      bikini=false;
+      zapatillas=false;
+      //visitados = {false};
+      numGiros = 0;
+      tamanioMapa = size;
+      last_action = actIDLE;
+      contador_visitados_otra = 0;
+      /*for (int i = 0;i < 3 ; i++){
+        for (int j = 0; j < tamanioMapa; j++ ){
+          mapaResultado[i][j] = 'P';
+          mapaResultado[tamanioMapa-i-1][j] = 'P';
+          mapaResultado[j][i] = 'P';
+          mapaResultado[j][tamanioMapa-i-1] = 'P';
+        }
+      }*/
+      
 
     }
 
@@ -34,8 +53,17 @@ class ComportamientoJugador : public Comportamiento{
   private:
   // Declarar aquí las variables de estado
   state current_state; 
-  Orientacion brujula;
+  //Orientacion brujula;
   Action last_action;
-  bool girar_derecha, bien_situado;
+  bool girar_derecha, bien_situado,bikini,zapatillas,recargando,girar_45_grados;
+  //bool visitados[MAX+1][MAX+1];
+  int contador_visitados_otra;
+  const int bateria_baja = 2000;
+  const int bateria_muy_baja = 500;
+  const int bateriaRecarga = 4500;
+  const int vidaBaja = 1000;
+  const int factorVidaBateria = 3;
+  int numGiros;
+  int tamanioMapa;
 };
 #endif
